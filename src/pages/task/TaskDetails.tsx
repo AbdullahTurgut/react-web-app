fe27702f3713e1cc792d7169ee8095cfdb109dc2
Abdullah Turgut <abdullah.trgt4196@gmail.com>
2025-05-11 01:26:45 +0300

    Completed delete functionality

--- src/pages/task/TaskDetails.tsx
+++ src/pages/task/TaskDetails.tsx
@@ -1,18 +1,53 @@
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { formatStatus } from "../../utils/CurrencyUtils";
 import { getFormatTaskDate } from "../../utils/DateUtils";
 import useTaskByTaskId from "../../hooks/task/useTaskByTaskId";
+import ConfirmDialog from "../../components/task/ConfirmDialog";
+import { useState } from "react";
+import { deleteTaskByTaskId } from "../../services/task/task-service";
 
 const TaskDetails = () => {
   const { taskId } = useParams<{ taskId: string }>();
-  const { task, errorMessage, isLoading } = useTaskByTaskId(taskId as string);
+  const [showDialog, setShowDialog] = useState<boolean>(false);
+  const navigate = useNavigate();
+  const { task, errorMessage, isLoading, setIsLoading, setErrorMessage } =
+    useTaskByTaskId(taskId as string);
+
+  const handleCancel = () => {
+    console.log("Cancel clicked");
+    setShowDialog(false);
+  };
+
+  const handleConfirm = () => {
+    setIsLoading(true);
+    deleteTaskByTaskId(taskId!)
+      .then((response) => {
+        console.log("Task deleted successfully:", response.data);
+        if (response && response.status === 204) {
+          navigate("/"); // Redirect to the task list or home page after deletion
+        }
+      })
+      .catch((error) => {
+        console.error("Error deleting task:", error);
+        setErrorMessage(error.message);
+      })
+      .finally(() => {
+        setIsLoading(false);
+        setShowDialog(false);
+      });
+  };
 
   return (
     <div className="container mt-2">
       {isLoading && <p>Loading...</p>}
       {errorMessage && <p className="text-danger">{errorMessage}</p>}
       <div className="d-flex flex-row-reverse mb-2">
-        <button className="btn btn-sm btn-danger">Delete</button>
+        <button
+          className="btn btn-sm btn-danger"
+          onClick={() => setShowDialog(true)}
+        >
+          Delete
+        </button>
         <button className="btn btn-sm btn-warning mx-2">Edit</button>
         <Link className="btn btn-sm btn-secondary" to={"/"}>
           Back
@@ -38,6 +73,13 @@ const TaskDetails = () => {
           </table>
         </div>
       </div>
+      <ConfirmDialog
+        title="Confirm Delete"
+        message="Are you sure want to delete this item ? "
+        show={showDialog}
+        onCancel={handleCancel}
+        onConfirm={handleConfirm}
+      />
     </div>
   );
 };
